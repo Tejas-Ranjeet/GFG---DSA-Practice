c935090931810fad84505a98fc5a0d8ccb8b8da0
Tejas Ranjeet <tejasranjeet03@gmail.com>
2026-07-21 00:19:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Min and Max in Array/min-and-max-in-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    vector<int> getMinMax(vector<int> &arr) {
+        
+        int maxi = INT_MIN;
+        int mini = INT_MAX;
+        
+        for(int i = 0; i < arr.size(); i++){
+            if(arr[i] > maxi){
+                maxi = arr[i];
+            }
+            if(arr[i]< mini){
+                mini = arr[i];
+            }
+        }
+        return {mini,maxi};
+        
+    }
+};
