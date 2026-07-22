2907c4ad4343944dd42f5f242944317c74b55b74
Tejas Ranjeet <tejasranjeet03@gmail.com>
2026-07-22 23:45:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Palindrome Number/palindrome-number.c
@@ -0,0 +1,13 @@
+int isPalindrome(int n) {
+    int num = abs(n);
+    int original = num;
+    int reverse = 0;
+
+    while (num > 0) {
+        int digit = num % 10;
+        reverse = reverse * 10 + digit;
+        num /= 10;
+    }
+
+    return original == reverse;
+}
